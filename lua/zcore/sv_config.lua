36a85f7b2e17014a8d3c05a0823044852a6c8b9b
Zach Cheatham <zachcheatham@gmail.com>
2016-12-07 23:06:49 -0600

    Use data-folder configuration.

--- lua/zcore/sv_config.lua
+++ lua/zcore/sv_config.lua
@@ -6,4 +6,10 @@ ZCore.Config.SQL.hostname 	= ""
 ZCore.Config.SQL.username	= ""
 ZCore.Config.SQL.password	= ""
 ZCore.Config.SQL.database	= ""
-ZCore.Config.SQL.port		= 3306
+
+if not ULib.fileExists("data/zcore/config_mysql.txt") then
+    ULib.fileCreateDir("data/zcore")
+    ULib.fileWrite("data/zcore/config_mysql.txt", ULib.makeKeyValues(ZCore.Config.SQL))
+else
+    ZCore.Config.SQL = ULib.parseKeyValues(ULib.fileRead("data/zcore/config_mysql.txt"))
+end
--- lua/zcore/sv_mysql.lua
+++ lua/zcore/sv_mysql.lua
@@ -9,12 +9,12 @@ local queryCache = {}
 
 local function connect()
 	if not mysqloo then Error("[ZCore] MySQLOO isn't installed properly. Unable to use MySQL functions.\n") end
-	
+
 	ServerLog("[ZCore] Connecting to MySQL...\n")
 	database = mysqloo.connect(ZCore.Config.SQL.hostname, ZCore.Config.SQL.username, ZCore.Config.SQL.password, ZCore.Config.SQL.database)
 
 	if timer.Exists("zcore_sql_connection_state") then timer.Destroy("zcore_sql_connection_state") end
-	
+
 	database.onConnectionFailed = function(_, msg)
 		ErrorNoHalt("[ZCore] Failed to connect to MySQL! " .. tostring(msg) .. "\n")
 		if previouslyConnected then
@@ -22,7 +22,7 @@ local function connect()
 			timer.Simple(30, connect)
 		end
 	end
-	
+
 	database.onConnected = function()
 		connected = true
 		ServerLog("[ZCore] Connected to MySQL.\n")
@@ -31,7 +31,7 @@ local function connect()
 			ZCore.MySQL.query(query[1], query[2])
 		end
 		table.Empty(queryCache)
-		
+
 		timer.Create("zcore_sql_connection_state", 60, 0, function()
 			if (database and database:status() == mysqloo.DATABASE_NOT_CONNECTED) then
 				connected = false
@@ -41,14 +41,18 @@ local function connect()
 				connect()
 			end
 		end)
-		
+
 		hook.Call("ZCore_MySQL_Connected", _, not previouslyConnected)
 		previouslyConnected = true
 	end
-	
+
 	database:connect()
 end
-connect() 
+if string.len(ZCore.Config.SQL.hostname) > 0 then
+    connect()
+else
+    ServerLog("[ZCore] In order to use MySQL, please set the configuration in data/zcore/config_mysql.txt\n")
+end
 
 function ZCore.MySQL.query(sql, callback)
 	if not connected then
@@ -61,7 +65,7 @@ function ZCore.MySQL.query(sql, callback)
 				callback(ZCore.MySQL.cleanSQLArray(data), q:lastInsert())
 			end
 		end
-		
+
 		function q:onError(err)
 			if err == "MySQL server has gone away" then
 				ServerLog("[ZCore] Caching query while MySQL is down...\n")
@@ -72,7 +76,7 @@ function ZCore.MySQL.query(sql, callback)
 				ErrorNoHalt(errStr .. "\n")
 			end
 		end
-		
+
 		q:start()
 	end
 end
@@ -93,13 +97,13 @@ end
 
 function ZCore.MySQL.cleanSQLArray(data)
 	if not type(data) == "table" then return data end
-	
+
 	local newData = {}
-	
+
 	for k, v in pairs(data) do
 		newData[k] = ZCore.MySQL.cleanSQLRow(v)
 	end
-	
+
 	return newData
 end
 
@@ -107,7 +111,7 @@ function ZCore.MySQL.cleanSQLRow(data)
 	if not data then return data end
 
 	local newData = table.Copy(data)
-	
+
 	-- REMOVE NULLS
 	local toRemove = {}
 	for k,v in pairs(newData) do
@@ -115,17 +119,17 @@ function ZCore.MySQL.cleanSQLRow(data)
 			table.insert(toRemove, k)
 		end
 	end
-	
+
 	for _,k in ipairs(toRemove) do
 		newData[k] = nil
 	end
-	
+
 	-- TURN NUMBERS INTO NUMBERS!
 	for k,v in pairs(newData) do
 		if tonumber(v) ~= nil then
 			newData[k] = tonumber(v)
 		end
 	end
-	
+
 	return newData
 end
