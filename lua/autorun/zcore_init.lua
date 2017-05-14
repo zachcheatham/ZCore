9e2976058502e8c6c6c1dea2fd2c1597672d0a08
Zach Cheatham <zachcheatham@gmail.com>
2017-05-14 17:28:05 -0500

    Remove permissions reference from init.

--- lua/autorun/zcore_init.lua
+++ lua/autorun/zcore_init.lua
@@ -1,14 +1,10 @@
 ZCore = {}
 
 if SERVER then
-	AddCSLuaFile("zcore/sh_permissions.lua")
-
 	include("zcore/sv_config.lua")
 	include("zcore/sv_mysql.lua")
 	include("zcore/sv_ulx.lua")
 	include("zcore/sv_util.lua")
 end
-
-include ("zcore/sh_permissions.lua")
 
 hook.Call("ZCore_PostInit")
